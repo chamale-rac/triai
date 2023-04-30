e9a2b076cb8371d818bc79568e6e6d3896a6da50
chamale-rac <samuel.chamale23@gmail.com>
2023-04-30 02:24:03 -0600

    Final API for production

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -62,13 +62,16 @@ const Customizer = () => {
 		try {
 			// IA API call
 			setGeneratingImg(true)
-			const response = await fetch('http://localhost:8080/api/v1/dalle', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
+			const response = await fetch(
+				'https://ia-powered-3d-api.onrender.com/api/v1/dalle',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ prompt }),
 				},
-				body: JSON.stringify({ prompt }),
-			})
+			)
 			const data = await response.json()
 			handleDecals(type, `data:image/png;base64,${data.photo}`)
 		} catch (error) {
