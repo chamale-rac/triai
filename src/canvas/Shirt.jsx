97ac30d0361a09fe006b08aeef06e1d806c3709f
Samuel Chamalé <samuel.chamale23@gmail.com>
2023-08-30 13:18:25 -0600

    fix: updated drei

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
 import { easing } from 'maath'
 import { useSnapshot } from 'valtio'
 import { useFrame } from '@react-three/fiber'
@@ -8,13 +9,19 @@ import state from '../store'
 
 const Shirt = () => {
 	const snap = useSnapshot(state)
-	const { nodes, materials } = useGLTF('./shirt_baked.glb')
+	const water_bottle = useGLTF('./water_bottle.glb')
+	const shirt_baked = useGLTF('./shirt_baked.glb')
 
 	const logoTexture = useTexture(snap.logoDecal)
 	const fullTexture = useTexture(snap.fullDecal)
 
 	useFrame((state, delta) =>
-		easing.dampC(materials.lambert1.color, snap.color, 0.25, delta),
+		easing.dampC(
+			water_bottle.materials.Water_Bottle_Yellow_Part.color,
+			snap.color,
+			0.25,
+			delta,
+		),
 	)
 
 	const stateString = JSON.stringify(snap)
@@ -23,8 +30,55 @@ const Shirt = () => {
 		<group key={stateString}>
 			<mesh
 				castShadow
-				geometry={nodes.T_Shirt_male.geometry}
-				material={materials.lambert1}
+				geometry={water_bottle.nodes.Water_Bottle_Lid.geometry}
+				material={water_bottle.materials.Water_Bottle_Metal_Part}
+				material-roughness={1}
+				dispose={null}
+				scale={0.038}
+				position={[0, 8.08, 0]}
+			/>
+			<mesh
+				castShadow
+				geometry={water_bottle.nodes.Water_Bottle_Metal_Part.geometry}
+				material={water_bottle.materials.Water_Bottle_Metal_Part}
+				material-roughness={1}
+				dispose={null}
+				scale={0.038}
+				position={[0, 8.08, 0]}
+			/>
+			<mesh
+				castShadow
+				geometry={water_bottle.nodes.Water_Bottle_Yellow_Part.geometry}
+				material={water_bottle.materials.Water_Bottle_Yellow_Part}
+				material-roughness={1}
+				dispose={null}
+				scale={0.038}
+				position={[0, 8.08, 0]}
+			>
+				{snap.isFullTexture && (
+					<Decal
+						position={[0, 8.4, 0]}
+						rotation={[0, 0, 0]}
+						scale={16.9}
+						map={fullTexture}
+					/>
+				)}
+				{snap.isLogoTexture && (
+					<Decal
+						position={[0, 2, 0.1]}
+						rotation={[0, 0, 0]}
+						scale={6}
+						map={logoTexture}
+						anisotropy={16}
+						depthTest={false}
+						depthWrite={true}
+					/>
+				)}
+			</mesh>
+			{/* <mesh
+				castShadow
+				geometry={shirt_baked.nodes.T_Shirt_male.geometry}
+				material={shirt_baked.materials.lambert1}
 				material-roughness={1}
 				dispose={null}
 			>
@@ -38,16 +92,15 @@ const Shirt = () => {
 				)}
 				{snap.isLogoTexture && (
 					<Decal
-						position={[0, 0.04, 0.15]}
+						position={[0, 0.1, 0.02]}
 						rotation={[0, 0, 0]}
 						scale={0.15}
 						map={logoTexture}
-						map-anisotropy={16}
 						depthTest={false}
 						depthWrite={true}
 					/>
 				)}
-			</mesh>
+			</mesh> */}
 		</group>
 	)
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
 	color: '#EFBD48',
 	isLogoTexture: true,
 	isFullTexture: false,
-	logoDecal: './threejs.png',
-	fullDecal: './threejs.png',
+	logoDecal: './uvg.png',
+	fullDecal: './uvg.png',
 })
 
 export default state
